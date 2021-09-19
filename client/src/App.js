4a9396a462941351475aaf8e00cfd094563322a0
moldir <moldirshb@gmail.com>
2021-09-19 18:34:44 -0400

    setting API

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 const BASE = process.env.REACT_APP_AIRTABLE_BASE;
 const KEY = process.env.REACT_APP_AIRTABLE_KEY;
-
+const URL = "https://api.airtable.com/v0/appdtddwvXlZlECIz/todo"
 
 function App() {
   return (
