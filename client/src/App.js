319717f0e384d3e348e4d3fb2dde3913296f784d
moldir <moldirshb@gmail.com>
2021-09-20 11:16:21 -0400

    components set

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,14 @@
 import './App.css';
+import Navbar from './components/Navbar'
+import { Route } from 'react-router-dom'
+// import Work from './components/Work'
+// import Health from './components/Health'
+// import Exercise from './components/Exercise'
+// import General from './components/General'
+import Home from './components/Home'
+// import AllTodo from './components/AllTodo'
+import axios from 'axios'
+
 const BASE = process.env.REACT_APP_AIRTABLE_BASE;
 const KEY = process.env.REACT_APP_AIRTABLE_KEY;
 const URL = "https://api.airtable.com/v0/appdtddwvXlZlECIz/todo"
@@ -6,7 +16,25 @@ const URL = "https://api.airtable.com/v0/appdtddwvXlZlECIz/todo"
 function App() {
   return (
     <div className="App">
-  {KEY} {BASE}
+        <Navbar />
+      <Route exact path="/">
+        < Home />
+      </Route>
+      <Route path="/">
+        <div> Work</div>
+      </Route>
+      <Route path="/">
+        <div> Health </div>
+      </Route>
+      <Route path="/">
+        <div> Exercise </div>
+      </Route>
+      <Route path="/">
+        <div> General </div>
+      </Route>
+      <Route path="/">
+        <div> AllTodo </div>
+    </Route>
     </div>
   );
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom"
+export default function Navbar() {
+  return (
+    <div>
+      {/* <Link to="/"> */}
+      <h1>What's your plan for today?</h1>
+      {/* </Link>
+     <Link to='/'> Home</Link>
+      <Link to='/new'> NewTodo </Link> */}
+    </div>
+  )
+}
